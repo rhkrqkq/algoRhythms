fbb02bfec7f8f55bc0cd02b522b074ac36c79cca
rhkrqkq <rhkrqkq1025@gmail.com>
2026-07-01 17:06:07 +0900

    070101

---
+++ programmers/level0/181910.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=my_string.length()-n; i<my_string.length(); i++) {
+            sb.append(my_string.charAt(i));
+        }
+        return sb.toString();
+    }
+}
